97dd24443488c5092ed9cd21cf80944c935450a3
EricWRogers <ericwilliamrogers@gmail.com>
2025-04-08 10:16:49 -0500

    Rename

--- Assets/scripts/mergeConflict.cs
+++ Assets/scripts/mergeConflict.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class mergeConflict : MonoBehaviour
+public class MergeConflict : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
