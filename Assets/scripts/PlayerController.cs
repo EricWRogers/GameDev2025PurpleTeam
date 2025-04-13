0d8a66f7f0b73f836fbcb9395db841463ac92dbb
CooperCantrell <coopercantrell56@gmail.com>
2025-04-13 14:59:20 -0500

    basic player controller done

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,6 +5,11 @@ public class PlayerController : MonoBehaviour
 {
         private Rigidbody2D body;
         public float speed = 10f;
+        public float jump = 50f;
+        public bool isJumping;
+        public Vector2 boxsize;
+        public float castDistinace;
+        public LayerMask groundLayer;
  
     private void Awake()
     {
@@ -15,7 +20,35 @@ public class PlayerController : MonoBehaviour
     {
         body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
  
-        if (Input.GetKey(KeyCode.Space))
-            body.AddForce(new Vector2(body.linearVelocity.x,speed));
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
+            body.AddForce(new Vector2(body.linearVelocity.x,jump));
     }
+
+
+    public bool isGrounded(){
+        if(Physics2D.BoxCast(transform.position,boxsize,0, -transform.up,castDistinace, groundLayer )) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position-transform.up * castDistinace, boxsize );
+    }
+    //  private void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     if(other.gameObject.CompareTag("Ground")){
+    //         isJumping = false;
+    //     }
+    // }
+
+    // private void OnCollisionExit2D(Collision2D other)
+    // {
+    //      if(other.gameObject.CompareTag("Ground")){
+    //         isJumping = true;
+    //     }
+    // }
 }
